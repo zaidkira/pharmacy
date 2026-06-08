6f075a2f7db4be45be34dd9b9015b67fe8d92f6c
zaid <ammizaidghost@gmail.com>
2026-06-08 05:48:33 +0100

    style: adjust dashboard main container padding on mobile devices for better phone responsiveness

--- src/app/layouts/dashboard-layout.tsx
+++ src/app/layouts/dashboard-layout.tsx
@@ -256,7 +256,7 @@ export function DashboardLayout() {
         )}
 
         {/* Main Content */}
-        <main className="flex-1 p-6 lg:p-8">
+        <main className="flex-1 p-4 sm:p-6 lg:p-8">
           <Outlet />
         </main>
       </div>
